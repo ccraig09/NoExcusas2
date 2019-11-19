41495be9e89511517adbaa249217d99675ff2d63
ccraig09 <35668007+ccraig09@users.noreply.github.com>
2019-11-19 12:03:14 -0400

    admob adding in

--- app/src/main/java/com/fchw/noexcusas/AboutUsActivity.java
+++ app/src/main/java/com/fchw/noexcusas/AboutUsActivity.java
@@ -9,15 +9,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class AboutUsActivity extends Fragment {
+    private TextView clickwap, clickFB;
+
+    public AboutUsActivity(){
+}
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.activity_about_us, container, false);
 
-
+        clickFB = view.findViewById(R.id.clickfb);
+        clickwap = view.findViewById(R.id.clickwapp);
 
         return view;
         }
