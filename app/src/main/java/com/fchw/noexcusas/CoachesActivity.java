9a3a4bd5643ecaa5e1cbf26ccdf65f25510bdb43
ccraig09 <35668007+ccraig09@users.noreply.github.com>
2019-12-27 22:02:57 -0400

    large heap true (temp mem fix)

--- app/src/main/java/com/fchw/noexcusas/CoachesActivity.java
+++ app/src/main/java/com/fchw/noexcusas/CoachesActivity.java
@@ -9,14 +9,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class CoachesActivity extends Fragment {
+    private ImageView joseIv, kikiIV, carlosIV;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.activity_coaches,container, false);
 
+        joseIv = view.findViewById(R.id.jose);
+        kikiIV = view.findViewById(R.id.kiki);
+        carlosIV = view.findViewById(R.id.carlos);
+
+
+        Picasso.get()
+                .load("https://i.imgur.com/pn0iWI6.jpg")
+                .placeholder(R.drawable.ic_cloud)
+                .into(joseIv);
+
+        Picasso.get()
+                .load("https://i.imgur.com/DHNBAH0.png")
+                .placeholder(R.drawable.ic_cloud)
+                .into(kikiIV);
+
+
+        Picasso.get()
+                .load("https://i.imgur.com/gTGHHqu.png")
+                .placeholder(R.drawable.ic_cloud)
+                .into(carlosIV);
 
 
 
